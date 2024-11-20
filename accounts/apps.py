ef1609501264211f181302260ee05d8bc276c40d
KimYehyun <tinyfeather@naver.com>
2024-11-20 17:24:16 +0900

    24.11.20 17:23 KYH-git에서 clone 받을때 accounts app에서 migrate 관련 문제 생긴거 해결

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,11 +1,15 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
 
     def ready(self):
-        # 장르 목록을 자동으로 업데이트
+        # 마이그레이션 완료 후 장르 목록 업데이트
+        post_migrate.connect(self.update_genres, sender=self)
+
+    def update_genres(self, sender, **kwargs):
         from accounts.models import Genre
         Genre.update_genres_from_tmdb()
