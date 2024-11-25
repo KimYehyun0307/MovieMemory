91dc321e948484a0333f1df8ee168ff3edf5dc30
KimYehyun <tinyfeather@naver.com>
2024-11-25 13:15:49 +0900

    24.11.25 13:15 KYH-게시판 작성중, static 수정

--- accounts/apps.py
+++ accounts/apps.py
@@ -16,7 +16,8 @@ def load_fixtures(sender, **kwargs):
     fixtures.json 데이터를 로드합니다.
     """
     try:
-        call_command('loaddata', 'accounts/fixtures/accounts/genre.json')  
-        print("Genres fixtures loaded successfully.")
+        call_command('loaddata', 'accounts/fixtures/accounts/genre.json')
+        call_command('loaddata', 'accounts/fixtures/accounts/users.json')
+        print("Genres fixtures and Users samples loaded successfully.")
     except Exception as e:
         print(f"Error loading fixtures: {e}")
--- communities/forms.py
+++ communities/forms.py
@@ -1,17 +1,22 @@
 from django import forms
-from .models import MovieReview, Comment, BambooPost, EventParticipation
+from .models import MovieReview, Comment, CommentReply, BambooPost, EventParticipation
 
 
 class MovieReviewForm(forms.ModelForm):
     class Meta:
         model = MovieReview
-        fields = ['movie', 'title', 'content', 'rating', 'is_comment_enabled', 'image']  # image 필드 추가
+        fields = ['title', 'content', 'rating', 'is_comment_enabled', 'image']  # image 필드 추가
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['review', 'content', 'is_reply', 'image']  # image 필드 추가
+        fields = ['content', 'is_reply', 'image']
+
+class CommentReplyForm(forms.ModelForm):
+    class Meta:
+        model = CommentReply
+        fields = ['content', 'image']
 
 
 class BambooPostForm(forms.ModelForm):
