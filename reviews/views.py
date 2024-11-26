978c2aded339b93ecb2f5048412f877a4bf17fe6
Kim DongCheol <kdc7132@gmail.com>
2024-11-26 09:40:52 +0900

    댓글 닉네임 비동기처리

--- reviews/views.py
+++ reviews/views.py
@@ -103,7 +103,7 @@ def add_comment(request, review_id):
         return JsonResponse({
             'comment': {
                 'id': comment.id,
-                'username': comment.user.username,
+                'username': comment.user.nickname,
                 'content': comment.content,
                 'created_at': comment.created_at.strftime('%Y-%m-%d %H:%M'),
             }
